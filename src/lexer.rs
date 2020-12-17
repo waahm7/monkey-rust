37573ac3b8bd673cc47ca6a54201b5ed8a2ec455
Waqar Ahmed <waahm7@gmail.com>
2020-12-18 00:12:42 +0500

    adds supports for Identifier and numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,7 @@
-use crate::token::{Token, TokenType};
+#![allow(dead_code)]
+
+use crate::token::{self, Token, TokenType};
+use std::vec::Vec;
 struct Lexer {
     input: String,
     position: usize,
@@ -29,7 +32,9 @@ impl Lexer {
     }
 
     pub fn next_token(&mut self) -> Token {
-        let mut tok: Token;
+        //ignore spaces
+        Lexer::skip_whitespace(self);
+
         let tok = match self.ch {
             '=' => Token {
                 type_token: TokenType::ASSIGN,
@@ -67,6 +72,18 @@ impl Lexer {
                 type_token: TokenType::EOF,
                 literal: String::new(),
             },
+            ch if ch.is_digit(10) => return Token {
+                type_token: TokenType::INT,
+                literal: Lexer::read_number(self),
+            },
+            //match identifier start
+            ch if Lexer::is_letter(ch) =>  {
+                let literal = Lexer::read_identifier(self);
+                return Token {
+                    type_token: token::lookup_ident(&literal),
+                    literal,
+                }
+            },
             _ => Token {
                 type_token: TokenType::ILLEGAL,
                 literal: self.ch.to_string(),
@@ -77,6 +94,34 @@ impl Lexer {
 
         tok
     }
+    fn read_number(&mut self) -> String {
+        let mut vector = Vec::new();
+        while self.ch.is_digit(10) {
+            vector.push(self.ch);
+            self.read_char();
+        }
+        vector.into_iter().collect()
+    }
+    fn read_identifier(&mut self) -> String {
+        let mut vector = Vec::new();
+        while Lexer::is_letter(self.ch) {
+            vector.push(self.ch);
+            self.read_char();
+        }
+
+        vector.into_iter().collect()
+    }
+
+    fn is_letter(ch: char) -> bool {
+        ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == ' ' || self.ch == '\n' || self.ch == '\r' || self.ch == '\t' {
+            self.read_char();
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -132,4 +177,175 @@ mod tests {
             assert_eq!(tok.literal, e.literal);
         }
     }
+
+
+    #[test]
+    fn test_next_token32() {
+        let input = String::from("let five = 5;
+let ten = 10;
+   let add = fn(x, y) {
+     x + y;
+};
+   let result = add(five, ten);
+");
+        let result = vec![
+            Token {
+                literal: "let".to_string(),
+                type_token: TokenType::LET,
+
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "five".to_string(),
+            },
+            Token {
+                type_token: TokenType::ASSIGN,
+                literal: "=".to_string(),
+            },
+            Token {
+                type_token: TokenType::INT,
+                literal: "5".to_string(),
+            },
+            Token {
+                type_token: TokenType::SEMICOLON,
+                literal: ";".to_string(),
+            },
+            Token {
+                type_token: TokenType::LET,
+                literal: "let".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "ten".to_string(),
+            },
+            Token {
+                type_token: TokenType::ASSIGN,
+                literal: "=".to_string(),
+            },
+            Token {
+                type_token: TokenType::INT,
+                literal: "10".to_string(),
+            },
+            Token {
+                type_token: TokenType::SEMICOLON,
+                literal: ";".to_string(),
+            },
+            Token {
+                type_token: TokenType::LET,
+                literal: "let".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "add".to_string(),
+            },
+            Token {
+                type_token: TokenType::ASSIGN,
+                literal: "=".to_string(),
+            },
+            Token {
+                type_token: TokenType::FUNCTION,
+                literal: "fn".to_string(),
+            },
+            Token {
+                type_token: TokenType::LPAREN,
+                literal: "(".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "x".to_string(),
+            },
+            Token {
+                type_token: TokenType::COMMA,
+                literal: ",".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "y".to_string(),
+            },
+            Token {
+                type_token: TokenType::RPAREN,
+                literal: ")".to_string(),
+            },
+            Token {
+                type_token: TokenType::LBRACE,
+                literal: "{".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "x".to_string(),
+            },
+            Token {
+                type_token: TokenType::PLUS,
+                literal: "+".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "y".to_string(),
+            },
+            Token {
+                type_token: TokenType::SEMICOLON,
+                literal: ";".to_string(),
+            },
+            Token {
+                type_token: TokenType::RBRACE,
+                literal: "}".to_string(),
+            },
+            Token {
+                type_token: TokenType::SEMICOLON,
+                literal: ";".to_string(),
+            },
+            Token {
+                type_token: TokenType::LET,
+                literal: "let".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "result".to_string(),
+            },
+            Token {
+                type_token: TokenType::ASSIGN,
+                literal: "=".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "add".to_string(),
+            },
+            Token {
+                type_token: TokenType::LPAREN,
+                literal: "(".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "five".to_string(),
+            },
+            Token {
+                type_token: TokenType::COMMA,
+                literal: ",".to_string(),
+            },
+            Token {
+                type_token: TokenType::IDENT,
+                literal: "ten".to_string(),
+            },
+            Token {
+                type_token: TokenType::RPAREN,
+                literal: ")".to_string(),
+            },
+            Token {
+                type_token: TokenType::SEMICOLON,
+                literal: ";".to_string(),
+            },
+            Token {
+                type_token: TokenType::EOF,
+                literal: String::new(),
+            },
+        ];
+
+        let mut lex = Lexer::new(input);
+        for (_, e) in result.iter().enumerate() {
+            let tok = lex.next_token();
+            assert_eq!(tok.type_token, e.type_token);
+            assert_eq!(tok.literal, e.literal);
+        }
+    }
+
 }
--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,7 @@
-#[derive(Debug,Eq, PartialEq)]
+#![allow(dead_code)]
+use std::collections::HashMap;
+
+#[derive(Debug,Eq, PartialEq, Clone, Hash)]
 pub enum TokenType {
     ILLEGAL,
     EOF,
@@ -24,6 +27,18 @@ pub enum TokenType {
     FUNCTION,
     LET,
 }
+
+pub  fn lookup_ident(ident: &str) -> TokenType {
+    //TODO: Move this out of function
+    let keywords: HashMap<&'static str, TokenType> = [("let", TokenType::LET), ("fn", TokenType::FUNCTION)]
+        .iter().cloned().collect();
+
+    if keywords.contains_key(ident) {
+        return keywords.get(ident).unwrap().clone();
+    }
+    TokenType::IDENT
+}
+
 #[derive(Debug)]
 pub struct Token {
     pub type_token: TokenType,
